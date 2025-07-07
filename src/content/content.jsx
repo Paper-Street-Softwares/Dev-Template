4fdd0e3e55897048e73af398e4005749ea3c1b03
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-07 15:57:51 -0300

    fix: rede social

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -97,7 +97,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "A_definir", // Sem o @
+  instagramProfile: "paulodruckadvogado", // Sem o @
   facebookProfile: "A_Definir",
   linkeDinProfile: "A_Definir",
   x: "A_Definir",
