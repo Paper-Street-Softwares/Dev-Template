a9a7b6eb332e35093823b6350443bed4fcddaa89
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-08 00:54:31 -0300

    fix: Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -123,20 +123,20 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADA EM SÃO PAULO - SP",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Direito com propósito e<span className="text-primary"> ação</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle:
+        "A gente não enrola: entende, age e resolve o que está travando sua vida.",
       heroImgPhone: heroImgPhone, // img da pessoa mobile
       heroDesktop: heroImgDesktop, //bg com foto da pessoa
       heroImg: bgHeroImg, //fundo da Hero
       heroDefaultImage: heroDefaultImg, //imagem da heroDefault Mobile
       backgroundPerson: backgroundPersonMobile,
-      ctaButtonText: "A_Definir",
+      ctaButtonText: "Resolver meu caso",
       obsHero: {
         icon: (
           <svg
@@ -156,7 +156,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "A_Definir",
+        text: "Atendimentos preferencialmente on-line. Presencial, apenas com horário agendado.",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
