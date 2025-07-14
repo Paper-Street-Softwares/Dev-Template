78dfce35570d6933df7051d41ff2c7ff5fa43157
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-14 09:25:58 -0300

    fix: Webp

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -1,14 +1,14 @@
-import logo from "../assets/imgs/logo/logo.png";
+import logo from "../assets/imgs/logo/logo.webp";
 import heroImgPhone from "../assets/imgs/hero/personImg.png";
 import heroImgDesktop from "../assets/imgs/hero/bgHeroInfluencer.jpg";
 import bgHeroImg from "../assets/imgs/hero/bgHeroInfluencer.jpg";
-import heroDefaultImg from "../assets/imgs/hero/lawHero.jpg";
+import heroDefaultImg from "../assets/imgs/hero/lawHero.webp";
 import backgroundPersonMobile from "../assets/imgs/hero/bgHeroMobile.jpg";
 
-import imgFeatures from "../assets/imgs/features/imgServices.jpg";
+import imgFeatures from "../assets/imgs/features/imgServices.webp";
 import imgDivisor from "../assets/imgs/features/imgDivisor.jpg";
 
-import aboutImg1 from "../assets/imgs/about/aboutImg.jpg";
+import aboutImg1 from "../assets/imgs/about/aboutImg.webp";
 import imgAbout1 from "../assets/imgs/about/aboutImg1.jpg";
 import imgAbout2 from "../assets/imgs/about/aboutImg2.jpg";
 import imgAbout3 from "../assets/imgs/about/aboutImg3.jpg";
@@ -17,7 +17,7 @@ import imgAbout5 from "../assets/imgs/about/aboutImg5.jpg";
 import aboutSocialImg from "../assets/imgs/about/aboutSocial.png";
 import aboutInstagram from "../assets/imgs/about/imgAboutInstagram.jpg";
 
-import imgSteps from "../assets/imgs/steps/imgSteps.jpg";
+import imgSteps from "../assets/imgs/steps/imgSteps.webp";
 
 import numbersImgBg from "../assets/imgs/paralaxe/bgParalaxe1.jpg";
 
@@ -420,9 +420,9 @@ const content = {
           eficazes para meus clientes.
           <br />
           <br />
-          Com mais de duas décadas de atuação na área do Direito, construí uma
-          trajetória pautada na ética, comprometimento e busca por soluções
-          eficazes para meus clientes.
+          Sou capacitada em conciliação e mediação conforme a Resolução 125/2010
+          do CNJ, atuando para resolver conflitos de forma pacífica, ágil e com
+          foco no diálogo.
           <br />
           <br />
           Minha missão é oferecer um atendimento humano, técnico e responsável,
