02b0117c6f2b4d613a28b0b03ba4d8a497b0f22e
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-25 17:31:51 -0300

    fix: Altera imagens temporárias

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -413,7 +413,7 @@ const content = {
         miniTag: "A_Definir",
         title: "A_Definir",
         subtitle: "A_Definir",
-        paragraph: <p>A_Definir</p>,
+        paragraph: <p></p>,
         labelInstagram: "Siga-nos no Instagram",
         labelFacebook: "Siga-nos no Facebook",
         labelLinkedin: "Siga-nos no LinkedIn",
