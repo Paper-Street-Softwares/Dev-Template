1c951ef9dc2a241992934847d4e322e38d7c6a2c
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-04 15:51:28 -0300

    fix: Navbar com parte debaixo transparent

--- src/components/sections/NavbarSocial.jsx
+++ src/components/sections/NavbarSocial.jsx
@@ -92,7 +92,7 @@ export default function NavbarSocial({ colorMode, mode }) {
     // default
     return scrolling
       ? "bg-gradient-to-b from-black to-bgSectionDark bg-opacity-100 shadow-lg border-b-[1px] border-primary"
-      : "bg-gradient-to-b from-black to-bgSectionDark border-b-[1px] border-none";
+      : "bg-gradient-to-b from-black to-transparent border-b-[1px] border-none";
   };
 
   return (
