c6bf50bcb904e3b5d9bd4b2e87e0cc021ae3feb4
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-04 14:30:01 -0300

    fix: infos content

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -53,14 +53,14 @@ import { FileText } from "lucide-react";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Dr. Enéas Pimentel",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
-  domain: "A_Definir", // Sem "www"
+  domain: "eneaspimentel.com.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "92",
+    firstPart: "98422", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "2503", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -75,9 +75,10 @@ export const infos = {
   endereco: (
     <div>
       <p>Endereço:</p>
-      <p>A_Definir</p>
-      <p>A_Definir</p>
-      <p>A_Definir</p>
+      <p>Rua São Hermes, 118, sala 02,</p>
+      <p>Monte das Oliveiras,</p>
+      <p>Manaus - AM,</p>
+      <p>CEP: 69093-817</p>
     </div>
   ),
   enderecoSecundario: (
@@ -92,11 +93,11 @@ export const infos = {
     <div>
       <p>Horário de expediente:</p>
       <p>Segunda a Sexta-feira</p>
-      <p>A_Definir</p>
+      <p>8h às 18h</p>
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "A_definir", // Sem o @
+  instagramProfile: "fh.eneas", // Sem o @
   facebookProfile: "A_Definir",
   linkeDinProfile: "A_Definir",
   x: "A_Definir",
