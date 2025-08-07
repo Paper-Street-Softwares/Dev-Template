374b39753b7e6a40d4869edbdec11a4da422061d
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-07 08:35:59 -0300

    fix: Revisa itens que sobraram no Template

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import Index from "./pages/Index.jsx";
 import Lp01 from "./pages/Lp01.jsx";
-import Lp02 from "./pages/Lp02.jsx";
-import Lp03 from "./pages/Lp03.jsx";
+
 import WhatsAppLinks from "./components/interactives/WhatsappLinks.jsx";
 
 // Layout fixo para o modo "site"
@@ -30,8 +29,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         {/* <Route path="/" element={<Lp01 />} /> */}
         {/* <Route path="/whatsapp" element={<WhatsAppLinks />} /> */}
 
-        {/* <Route path="/lp02" element={<Lp02 />} /> */}
-        {/* <Route path="/lp03" element={<Lp03 />} /> */}
         {/* <Route path="/whatsapp" element={<WhatsAppLinks form={true} />} /> */}
       </Routes>
     ) : (
