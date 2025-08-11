7956350318ebb1d171163800e2a4aabffda8b1d1
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-11 09:09:29 -0300

    fix: Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -133,20 +133,21 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADO EM MAGÉ - RJ",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Você confia na sua
+          <span className="text-primary"> defesa?</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle:
+        "Muitos só percebem a importância de um bom advogado quando já é tarde demais.",
       heroImgPhone: heroImgPhone, // img da pessoa mobile
       heroDesktop: heroImgDesktop, //bg com foto da pessoa
       heroImg: bgHeroImg, //fundo da Hero
       heroDefaultImage: heroDefaultImg, //imagem da heroDefault Mobile
       backgroundPerson: backgroundPersonMobile,
-      ctaButtonText: "A_Definir",
+      ctaButtonText: "Quero estar preparado",
       obsHero: {
         icon: (
           <svg
@@ -166,7 +167,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "A_Definir",
+        text: "Atendimento 24h em casos de prisão em flagrante.",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
