952678cf3f1f234a5a256b7a6ef9f6315a13dc0e
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-11 11:28:17 -0300

    fix: Ajustes de padding

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -30,11 +30,11 @@ export default function Index() {
       />
       <Features
         colorMode={colorMode}
-        defaultFeature={false}
+        defaultFeature={true}
         button={false}
         modalWithCards={false}
         paragraphs={false}
-        sixCards={true}
+        sixCards={false}
         paragraphsModal={false}
       />
       <About modal={true} showGallery={false} colorMode={colorMode} />
