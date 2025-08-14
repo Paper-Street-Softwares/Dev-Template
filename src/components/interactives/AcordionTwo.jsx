520a84f0f7d5726d22b17255e55e7782ef34f826
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-14 07:38:44 -0300

    fix: Remove muiIcons

--- src/components/interactives/AcordionTwo.jsx
+++ src/components/interactives/AcordionTwo.jsx
@@ -3,7 +3,6 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { ChevronDown } from "lucide-react";
 
 import content from "../../content/content";
