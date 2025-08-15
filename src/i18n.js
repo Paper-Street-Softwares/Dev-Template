9893f8071f195e2bdba81df266151582eb0a3dd3
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-15 16:46:27 -0300

    fix: Insere o Espanhol

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,9 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import contentPT from "./locales/pt.json"; // para textos já traduzidos, se quiser
-import contentEN from "./locales/en.json"; // idem
-import content from "./content/content"; // seu content.js
+import contentEN from "./locales/en.json";
+import contentES from "./locales/es.json";
+import content from "./content/content";
 
 // Função para extrair só os textos (ignora imagens ou JSX)
 const extractText = (obj) => {
@@ -28,10 +29,16 @@ const enTexts = {
   ...contentEN,
 };
 
+const esTexts = {
+  ...extractText(content.texts), // você pode substituir aqui pelos textos em inglês
+  ...contentES,
+};
+
 i18n.use(initReactI18next).init({
   resources: {
     pt: { translation: ptTexts },
     en: { translation: enTexts },
+    es: { translation: esTexts },
   },
   lng: "pt",
   fallbackLng: "pt",
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -40,6 +40,13 @@ export default function Index() {
         >
           Português
         </Button>
+        <Button
+          label="Espanhol"
+          size="small"
+          onClick={() => changeLanguage("es")}
+        >
+          Português
+        </Button>
       </div>
 
       <Navbar colorMode={colorMode} />
