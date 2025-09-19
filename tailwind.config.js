52551ca94c1442b62afd2d050fbed31d573c524f
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-19 17:15:52 -0300

    fix: fontes locais

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,10 +48,10 @@ export default {
         "custom-opacityButton": "-5px 5px 0px", // 30% de opacidade no Botão
       },
       fontFamily: {
-        mainFont: ["Merriweather", "sans-serif"],
-        secondFont: ["Merriweather", "sans-serif"],
-        // mainFont: ["Poppins", "sans-serif"],
-        // secondFont: ["Poppins", "sans-serif"],
+        // mainFont: ["Merriweather", "sans-serif"],
+        // secondFont: ["Merriweather", "sans-serif"],
+        mainFont: ["Poppins", "sans-serif"],
+        secondFont: ["Poppins", "sans-serif"],
       },
       fontSize: {
         paragraph1: "0.625rem",
