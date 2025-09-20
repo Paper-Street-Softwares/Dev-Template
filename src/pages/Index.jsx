910a28b1c390030dc40abb028dcf1725d55f1e06
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-20 09:38:39 -0300

    fix: Heros v1

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -58,9 +58,9 @@ export default function Index() {
       <Hero
         colorMode={colorMode}
         appDownloadButtons={false}
-        defaultHero={true}
+        defaultHero={false}
         influencer={false}
-        mesclado={false}
+        mesclado={true}
       />
 
       <Features
