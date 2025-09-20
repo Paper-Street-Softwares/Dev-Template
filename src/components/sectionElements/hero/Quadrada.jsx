e448692775c22f13ea7a054db5a3d6497d18d961
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-20 07:36:30 -0300

    fix: LCP imagem hero

--- src/components/sectionElements/hero/Quadrada.jsx
+++ src/components/sectionElements/hero/Quadrada.jsx
@@ -151,21 +151,23 @@ export default function Quadrada({ appDownloadButtons, colorMode }) {
                     alt={content.texts.hero.alt}
                     fetchpriority="high"
                     decoding="sync"
+                    width="930" // coloque as dimensões reais da imagem
+                    height="1287" // coloque as dimensões reais da imagem
                     className={`mt-[20px] desktop1:mt-0 rounded-xl w-full
-                    phone1:h-[300px] phone1:max-w-[300px]
-                    phone2:h-[355px] phone2:max-w-[355px]
-                    phone3:h-[405px] phone3:max-w-[405px]
-                    tablet1:h-[576px] tablet1:min-w-[576px]
-                    tablet2:h-[690px] tablet2:min-w-[690px]
-                    desktop1:h-[400px] desktop1:min-w-[200px]
-                    desktop2:h-[400px] desktop2:min-w-[400px]
-                    object-cover
-                    ${
-                      colorMode === "light"
-                        ? "desktop1:shadow-custom-opacity desktop1:shadow-shadowHero/10"
-                        : "shadow-custom-opacity shadow-shadowHero/10"
-                    }
-                  `}
+      phone1:h-[300px] phone1:max-w-[300px]
+      phone2:h-[355px] phone2:max-w-[355px]
+      phone3:h-[405px] phone3:max-w-[405px]
+      tablet1:h-[576px] tablet1:min-w-[576px]
+      tablet2:h-[690px] tablet2:min-w-[690px]
+      desktop1:h-[400px] desktop1:min-w-[200px]
+      desktop2:h-[400px] desktop2:min-w-[400px]
+      object-cover
+      ${
+        colorMode === "light"
+          ? "desktop1:shadow-custom-opacity desktop1:shadow-shadowHero/10"
+          : "shadow-custom-opacity shadow-shadowHero/10"
+      }
+    `}
                   />
                 </picture>
               </MotionDivDownToUp>
