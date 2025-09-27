c482ec438dc0397534fa18bb79fb6e572d2c8f3d
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-27 11:38:06 -0300

    fix: Infos content e rede social

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -64,14 +64,14 @@ import imgNews1Mobile from "../assets/imgs/news/imgNews1Mobile.webp";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Dra. Ingrid Magno",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
   domain: "ingridmagnoprevidenciario.com.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "91",
+    firstPart: "99303", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "4905", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -107,7 +107,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "", // Sem o @
+  instagramProfile: "ingridmagnojuridico", // Sem o @
   facebookProfile: "",
   linkeDinProfile: "",
   x: "",
