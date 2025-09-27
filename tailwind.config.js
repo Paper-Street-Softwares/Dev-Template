e3c53ff83477ad7df2b6d1cb3cd03ca68b93e243
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-27 11:13:55 -0300

    fix: Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,17 @@ export const theme = {
     colors: {
       // Principais
 
-      primary: "#058cd7",
-      buttonColor: "#058cd7",
-      minititleDark: "#058cd7", //bg minitag de seções claras
-      minititle: "#058cd7", //bg minitag de seções escuras
+      primary: "#84212A",
+      buttonColor: "#84212A",
+      minititleDark: "#84212A", //bg minitag de seções claras
+      minititle: "#84212A", //bg minitag de seções escuras
 
       secondary: "#000000",
 
       // Fundos de Seção
 
       bgSectionLight: "#FFFFFF",
-      bgSectionDark: "#990000",
+      bgSectionDark: "#FFFFFF",
       bgSectionOpacityLight: "#F7F7F7",
       bgFixedLight: "#E8E8E8",
       bgSectionOpacityDark: "#111111",
