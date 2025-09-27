1d3e4d926ed39e1d5d88c9d1e666456d1abb0f59
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-27 09:39:14 -0300

    fix: Defini fontes

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,9 +53,9 @@ export const theme = {
       "custom-opacityButton": "-5px 5px 0px", // 30% de opacidade no Botão
     },
     fontFamily: {
-      mainFont: ["Merriweather", "sans-serif"],
+      mainFont: ["Merriweather"],
       // mainFont: ["Poppins", "sans-serif"],
-      secondFont: ["Merriweather", "sans-serif"],
+      secondFont: ["Roboto", "sans-serif"],
       league: ["League Gothic"],
       barlow: ["Barlow Condensed"],
     },
