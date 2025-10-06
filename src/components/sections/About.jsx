4bb6174dd8fd7f0dbb3291e631de5a6206204a29
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-06 11:41:49 -0300

    fix: About e About Instagram

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -10,6 +10,7 @@ import ParagraphSemFading from "../sectionElements/about/ParagraphSemFading";
 import ParagraphWithFading from "../sectionElements/about/ParagraphWithFading";
 import content from "../../content/content";
 import GalleryAbout from "../sectionElements/about/GalleryAbout";
+import Button from "../interactives/Button";
 
 export default function About({ modal = true, showGallery, colorMode }) {
   const { t } = useTranslation();
@@ -75,6 +76,29 @@ export default function About({ modal = true, showGallery, colorMode }) {
               <ParagraphSemFading colorMode={colorMode} />
             )}
             {modal && <AboutModal colorMode={colorMode} />}
+            <Button
+              label="Baixar meu e-book"
+              buttonLink="https://p.eduzz.com/2725901"
+              className="mt-6"
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-book-down-icon lucide-book-down"
+                >
+                  <path d="M12 13V7" />
+                  <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20" />
+                  <path d="m9 10 3 3 3-3" />
+                </svg>
+              }
+            />
           </MotionDivDownToUp>
         </div>
       </SectionWrapper>
