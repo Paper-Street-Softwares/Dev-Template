410b7b9d99acb702301153db4c684e294385fa40
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-08 15:57:03 -0300

    fix: sidebar

--- src/components/sectionElements/SidebarSocial.jsx
+++ src/components/sectionElements/SidebarSocial.jsx
@@ -88,7 +88,7 @@ export default function SidebarSocial({ colorMode, mode = "blog" }) {
                     <img
                       src={content.texts.navbar.solidLogo.img}
                       alt={content.texts.navbar.solidLogo.alt}
-                      className="w-[50%] h-auto p-[5px]"
+                      className="w-[90%] h-auto p-[5px]"
                     />
                   </span>
                   <span>
