74086b27608e58f43eb2f21dca07b317b4890c53
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-14 07:12:20 -0300

    fix: infos index, sitemap, robots, metatags, infos content e rede social

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -64,14 +64,14 @@ import imgNews1Mobile from "../assets/imgs/news/imgNews1Mobile.webp";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Dr. Laudelino Souza Neto",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
-  domain: "A_Definir", // Sem "www"
+  domain: "laudelinosouzaneto.adv.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "71",
+    firstPart: "99159", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "4613", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -107,7 +107,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "", // Sem o @
+  instagramProfile: "advogado.laudelino", // Sem o @
   facebookProfile: "",
   linkeDinProfile: "",
   x: "",
