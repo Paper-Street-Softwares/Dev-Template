6a5c9d9fe5a139f1046529684d6e717f024bc367
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-17 08:26:53 -0300

    fix: Rede sociais do cartao

--- src/components/interactives/Cartao/CartaoRedeSocial.jsx
+++ src/components/interactives/Cartao/CartaoRedeSocial.jsx
@@ -11,7 +11,7 @@ const icons = {
       xmlns="http://www.w3.org/2000/svg"
       width={16}
       height={16}
-      fill="currentColor"
+      fill="white"
       viewBox="0 0 24 24"
     >
       <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.472-.148-.67.15-.197.297-.768.966-.94 1.164-.173.198-.347.223-.644.074-.297-.149-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.372-.025-.521-.075-.149-.669-1.611-.916-2.207-.242-.579-.487-.5-.67-.51-.173-.007-.372-.009-.571-.009-.198 0-.52.074-.793.372-.273.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.095 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.226 1.36.194 1.872.118.571-.085 1.758-.718 2.006-1.412.248-.694.248-1.288.173-1.412-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.896a9.825 9.825 0 012.893 6.994c-.002 5.45-4.436 9.884-9.884 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.158 11.892c0 2.096.547 4.142 1.588 5.94L0 24l6.305-1.654a11.882 11.882 0 005.732 1.463h.005c6.554 0 11.89-5.335 11.892-11.892a11.821 11.821 0 00-3.466-8.413" />
@@ -24,7 +24,7 @@ const icons = {
       height="16"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -41,7 +41,7 @@ const icons = {
       height="16"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -59,7 +59,7 @@ const icons = {
       height="16"
       viewBox="0 0 24 24"
       fill="transparent"
-      stroke="currentColor"
+      stroke="white"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -76,7 +76,7 @@ const icons = {
       height="16"
       viewBox="0 0 24 24"
       fill="transparent"
-      stroke="currentColor"
+      stroke="white"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -107,7 +107,7 @@ const icons = {
       height="16"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -125,7 +125,7 @@ const icons = {
       height="16"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -143,7 +143,7 @@ const icons = {
       height="16"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -158,7 +158,7 @@ const icons = {
       xmlns="http://www.w3.org/2000/svg"
       width={16}
       height={16}
-      fill="currentColor"
+      fill="white"
       viewBox="0 0 24 24"
     >
       <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.472-.148-.67.15-.197.297-.768.966-.94 1.164-.173.198-.347.223-.644.074-.297-.149-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.372-.025-.521-.075-.149-.669-1.611-.916-2.207-.242-.579-.487-.5-.67-.51-.173-.007-.372-.009-.571-.009-.198 0-.52.074-.793.372-.273.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.095 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.226 1.36.194 1.872.118.571-.085 1.758-.718 2.006-1.412.248-.694.248-1.288.173-1.412-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.896a9.825 9.825 0 012.893 6.994c-.002 5.45-4.436 9.884-9.884 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.158 11.892c0 2.096.547 4.142 1.588 5.94L0 24l6.305-1.654a11.882 11.882 0 005.732 1.463h.005c6.554 0 11.89-5.335 11.892-11.892a11.821 11.821 0 00-3.466-8.413" />
@@ -171,7 +171,7 @@ const icons = {
       height="16"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -188,7 +188,7 @@ const icons = {
       height="16"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -254,18 +254,18 @@ function CartaoRedeSocial({ tipo = "contato", socio }) {
 
   const labels = {
     github: "GitHub",
-    whatsapp: "WhatsApp",
+    whatsapp: content.texts.infos.phone,
     telefone: "Telefone",
     site: "Site",
-    email: "E-mail",
+    email: content.texts.infos.email,
     instagram: "Instagram",
     facebook: "Facebook",
     tiktok: "TikTok",
     x: "X",
     linkedin: "LinkedIn",
     githubSocial: "GitHub",
-    whatsappSocial: "WhatsApp",
-    emailSocial: "E-mail",
+    whatsappSocial: content.texts.infos.phone,
+    emailSocial: content.texts.socios.socio1.social.emailSocial,
   };
 
   const links =
@@ -373,7 +373,7 @@ function CartaoRedeSocial({ tipo = "contato", socio }) {
               aria-label={`Link para ${labels[key] || key}`}
             >
               <IconButtonCartao
-                label={labels[key]} // sempre mostra o nome completo da rede
+                label={tipo === "empresa" ? "" : labels[key]}
                 ariaLabel={`Botão para ${labels[key]}`}
                 icon={svg}
                 width={tipo === "empresa" ? "w-auto" : "min-w-[200px] m-auto"}
