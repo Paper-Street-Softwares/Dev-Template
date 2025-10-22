bac51231ad400cc66c082d6e0e5391a7ec433c8a
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-22 11:07:49 -0300

    fix: Infos e rede social

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -64,14 +64,14 @@ import imgNews1Mobile from "../assets/imgs/news/imgNews1Mobile.webp";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Ferreira Alves Advocacia",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
-  domain: "A_Definir", // Sem "www"
+  domain: "direitofamiliarista.adv.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "11",
+    firstPart: "94700", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "1941", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -107,7 +107,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "", // Sem o @
+  instagramProfile: "draclaudiaferreira.adv", // Sem o @
   facebookProfile: "",
   linkeDinProfile: "",
   x: "",
