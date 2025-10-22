6829d0ef58d6d386a0f6b61d3990d42db29491f8
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-22 09:26:44 -0300

    fix: revisao

--- src/components/sectionElements/SidebarSocial.jsx
+++ src/components/sectionElements/SidebarSocial.jsx
@@ -98,9 +98,7 @@ export default function SidebarSocial({ colorMode, mode = "blog" }) {
                       onClick={(e) => hide(e)}
                       rounded
                       outlined
-                      className={`${
-                        colorMode ? "text-secondary" : "text-secondary"
-                      }`}
+                      className={`${colorMode ? "text-white" : "text-white"}`}
                     >
                       <X size={32} />
                     </Button>
@@ -108,11 +106,11 @@ export default function SidebarSocial({ colorMode, mode = "blog" }) {
                 </div>
 
                 <div className="h-screen overflow-y-auto">
-                  <hr className="m-5 mx-3 border-top-1 surface-border border-secondary" />
+                  <hr className="m-5 mx-3 border-top-1 surface-border border-white" />
                   <ul className="p-3 m-0 list-none">
                     <li>
                       <ul
-                        className={`p-0 m-0 -mt-[16px] overflow-hidden font-medium text-secondary list-none text-paragraph3 font-secondFont`}
+                        className={`p-0 m-0 -mt-[16px] overflow-hidden font-medium text-white list-none text-paragraph3 font-secondFont`}
                       >
                         {visibleSections.map(({ id, label }, index) => (
                           <li key={id}>
