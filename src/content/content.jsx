2bb9d2e03f716df4750c9c3b192e959168cd3257
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-03 09:01:49 -0300

    fix: Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -64,14 +64,14 @@ import imgNews1Mobile from "../assets/imgs/news/imgNews1Mobile.webp";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Dr. Alex Pinheiro",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
   domain: "alexpinheiro.adv.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "12",
+    firstPart: "98195", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "5043", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -107,7 +107,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "", // Sem o @
+  instagramProfile: "alex.pinheiroadvogado", // Sem o @
   facebookProfile: "",
   linkeDinProfile: "",
   x: "",
