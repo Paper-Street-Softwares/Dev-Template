e882c73924d5df53c211b3c6d703c270da3baef3
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-05 14:58:21 -0300

    fix: Hero copy

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -595,7 +595,7 @@ const content = {
       img: imgSteps,
       labelCards: "Ver mais",
       label: "Clique aqui para ver todas as matérias",
-      blogLink: "A_Definir", //sem "https://"
+      blogLink: "sandyegodiasadv.wordpress.com", //sem "https://"
     },
     faq: {
       miniTag: "TIRE SUAS DÚVIDAS",
