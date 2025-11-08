4a96fc5cb6589d948b4a626002c61262cd1fa803
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-08 08:47:03 -0300

    fix: Infos content, metatags, sitemap, robots e infos content

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -64,14 +64,14 @@ import imgNews1Mobile from "../assets/imgs/news/imgNews1Mobile.webp";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "JM Advogados Associados",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
-  domain: "A_Definir", // Sem "www"
+  domain: "jmadvogadosassociados.com.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "11",
+    firstPart: "99614", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "1110", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
