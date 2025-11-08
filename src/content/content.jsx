45c2ea27294a34c3d2fdce283a0ed29f37ed6dc5
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-08 09:45:48 -0300

    fix: Infos index, metatags, sitemap, robots, infos content e rede social

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -64,14 +64,14 @@ import imgNews1Mobile from "../assets/imgs/news/imgNews1Mobile.webp";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Dra. Josana Magalhães",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
-  domain: "A_Definir", // Sem "www"
+  domain: "josanamagalhaes.com.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "86",
+    firstPart: "99944", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "7532", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -107,7 +107,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "", // Sem o @
+  instagramProfile: "josamagalhaes", // Sem o @
   facebookProfile: "",
   linkeDinProfile: "",
   x: "",
