8cbbbe00001e05bb925c1c3bede5707ea48c1aa7
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-11 11:54:15 -0300

    fix: Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -64,14 +64,14 @@ import imgNews1Mobile from "../assets/imgs/news/imgNews1Mobile.webp";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "AQ Engenharia e Arquitetura",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
-  domain: "A_Definir", // Sem "www"
+  domain: "aqengenhariaaf.com.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "66",
+    firstPart: "99626", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "7717", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -107,7 +107,7 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "", // Sem o @
+  instagramProfile: "engenharia_alvesqueiroz", // Sem o @
   facebookProfile: "",
   linkeDinProfile: "",
   x: "",
