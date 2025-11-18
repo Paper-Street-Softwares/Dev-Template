3646cd4ffd705abfda738b001dcbfb9fc2f12e64
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-11-18 16:33:44 -0300

    fix: Reseta

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,17 @@ export const theme = {
     colors: {
       // Principais
 
-      primary: "#c53f09",
-      buttonColor: "#c53f09",
-      minititleDark: "#c53f09", //bg minitag de seções claras
-      minititle: "#c53f09", //bg minitag de seções escuras
+      primary: "#0C0A0A",
+      buttonColor: "#0C0A0A",
+      minititleDark: "#0C0A0A", //bg minitag de seções claras
+      minititle: "#0C0A0A", //bg minitag de seções escuras
 
       secondary: "#000000",
 
       // Fundos de Seção
 
       bgSectionLight: "#FFFFFF",
-      bgSectionDark: "#003d75",
+      bgSectionDark: "#990000",
       bgSectionOpacityLight: "#F7F7F7",
       bgFixedLight: "#E8E8E8",
       bgSectionOpacityDark: "#111111",
