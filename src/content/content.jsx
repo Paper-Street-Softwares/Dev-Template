c28639210aa579560b62b12bde39fbda60d0baf0
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-11-27 10:44:15 -0300

    fix: infos content e rede social

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -64,14 +64,14 @@ import imgNews1Mobile from "../assets/imgs/news/imgNews1Mobile.webp";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Dr. Mauro Monção",
   email: "contato", // Email desejado pelo cliente
-  emailSecundario: "A_Definir", // Email desejado pelo cliente
+  emailSecundario: "mauromoncaofilho@gmail.com", // Email desejado pelo cliente
   domain: "mauromoncao.adv.br", // Sem "www"
   phone: {
-    ddd: "A_Definir",
-    firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "A_Definir", // Apenas os 4 últimos números
+    ddd: "86",
+    firstPart: "99482", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "0054", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
@@ -107,8 +107,8 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "", // Sem o @
-  facebookProfile: "",
+  instagramProfile: "mauromoncaoadvogados_", // Sem o @
+  facebookProfile: "61584312205991",
   linkeDinProfile: "",
   x: "",
   tiktokProfile: "",
