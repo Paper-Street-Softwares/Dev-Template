af73df0248f8d861c8d73292883ae93a85253c09
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-08 14:07:40 -0300

    fix: Tailwind

--- src/assets/context/ColorModeContext.jsx
+++ src/assets/context/ColorModeContext.jsx
@@ -1,18 +1,18 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext } from 'react'
 
-const ColorModeContext = createContext();
+const ColorModeContext = createContext()
 
 export function ColorModeProvider({ children }) {
-  const [colorMode, setColorMode] = useState("light"); // default, light ou dark
+  const [colorMode, setColorMode] = useState('default') // default, light ou dark
 
   return (
     <ColorModeContext.Provider value={{ colorMode, setColorMode }}>
       {children}
     </ColorModeContext.Provider>
-  );
+  )
 }
 
 // Hook para facilitar o uso
 export function useColorMode() {
-  return useContext(ColorModeContext);
+  return useContext(ColorModeContext)
 }
