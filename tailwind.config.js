5318035616b7eabdc381ffa73a72139de63c7c3b
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-16 09:36:33 -0300

    fix: Tailwind e Contraste

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,17 @@ export const theme = {
     colors: {
       // Principais
 
-      primary: '#0C0A0A',
-      buttonColor: '#0C0A0A',
-      minititleDark: '#0C0A0A', //bg minitag de seções claras
-      minititle: '#0C0A0A', //bg minitag de seções escuras
+      primary: '#D7B283',
+      buttonColor: '#D7B283',
+      minititleDark: '#D7B283', //bg minitag de seções claras
+      minititle: '#D7B283', //bg minitag de seções escuras
 
       secondary: '#000000',
 
       // Fundos de Seção
 
       bgSectionLight: '#FFFFFF',
-      bgSectionDark: '#990000',
+      bgSectionDark: '#132745',
       bgSectionOpacityLight: '#F7F7F7',
       bgFixedLight: '#E8E8E8',
       bgSectionOpacityDark: '#111111',
@@ -30,10 +30,10 @@ export const theme = {
       // Botões e Links
 
       links: '#0189BB',
-      iconButtons: '#fff',
-      labelButtons: '#fff',
-      minititleLightLabel: '#fff', //label das minitag de seções claras
-      minititleDarkLabel: '#fff', //label minitag de seções escuras
+      iconButtons: '#000',
+      labelButtons: '#000',
+      minititleLightLabel: '#000', //label das minitag de seções claras
+      minititleDarkLabel: '#000', //label minitag de seções escuras
 
       // Sombras
 
